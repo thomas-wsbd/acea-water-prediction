58a16630f796de76be8452cc592cd83b6e902996
esquire900 <simonnouwens@gmail.com>
2021-02-15 09:20:39 +0100

    quite some simulation results, makeing a shitshow of the file organization!

--- network.py
+++ network.py
@@ -19,6 +19,7 @@ from pytorch_lightning.loggers.csv_logs import CSVLogger
 # Plotting
 import matplotlib.pyplot as plt
 
+
 class TimeseriesDataset(Dataset):
     '''
     Custom Dataset subclass.
@@ -28,16 +29,18 @@ class TimeseriesDataset(Dataset):
       of `(batch_size, seq_len, n_features)` shape.
     Suitable as an input to RNNs.
     '''
+
     def __init__(self, X: np.ndarray, y: np.ndarray, seq_len: int = 1):
         self.X = torch.tensor(X).float()
         self.y = torch.tensor(y).float()
         self.seq_len = seq_len
 
     def __len__(self):
-        return self.X.__len__() - (self.seq_len-1)
+        return self.X.__len__() - (self.seq_len - 1)
 
     def __getitem__(self, index):
-        return (self.X[index:index+self.seq_len], self.y[index+self.seq_len-1])
+        return (self.X[index:index + self.seq_len], self.y[index + self.seq_len - 1])
+
 
 class MyDataModule(pl.LightningDataModule):
     '''
@@ -48,7 +51,8 @@ class MyDataModule(pl.LightningDataModule):
       and processing work in one place.
     '''
 
-    def __init__(self, features, target, seq_len=1, batch_size=128, num_workers=0, split=8000, pred_ahead=30):
+    def __init__(self, X_train, X_test, y_train, y_test, seq_len=1, batch_size=128, num_workers=0, split=8000,
+                 pred_ahead=30):
         super().__init__()
         self.seq_len = seq_len
         self.batch_size = batch_size
@@ -62,41 +66,14 @@ class MyDataModule(pl.LightningDataModule):
         self.columns = None
         self.preprocessing = None
         self.testsize = 0
-        
-        self.features = features
-        self.target = target
+
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.y_train = y_train
         self.split = split
-        self.pred_ahead = pred_ahead
 
-    def prepare_data(self):
-        pass
 
-    def setup(self, mode):
-        from sklearn.preprocessing import MinMaxScaler, StandardScaler
-        
-        split = self.split
-        self.X_train = self.features[:split]
-        self.y_train = self.target[self.pred_ahead:split+self.pred_ahead]
-
-        self.X_val = self.features[split:-self.pred_ahead]
-        self.y_val = self.target[split+self.pred_ahead:]
-        
-        self.X_test = self.features[split:-self.pred_ahead]
-        self.y_test = self.target[split+self.pred_ahead:]
-        
-#         pca = PCA()
-        
-#         self.X_train = pca.fit_transform(self.X_train)
-#         self.X_val = pca.transform(self.X_val)
-#         self.X_test = pca.transform(self.X_test)
-        
-        scaler = StandardScaler()
-        
-        self.X_train = scaler.fit_transform(self.X_train)
-        self.X_val = scaler.transform(self.X_val)
-        self.X_test = scaler.transform(self.X_test)
-
-        
     def train_dataloader(self):
         train_dataset = TimeseriesDataset(self.X_train,
                                           self.y_train,
@@ -109,8 +86,8 @@ class MyDataModule(pl.LightningDataModule):
         return train_loader
 
     def val_dataloader(self):
-        val_dataset = TimeseriesDataset(self.X_val,
-                                        self.y_val,
+        val_dataset = TimeseriesDataset(self.X_test,
+                                        self.y_test,
                                         seq_len=self.seq_len)
         val_loader = DataLoader(val_dataset,
                                 batch_size=self.batch_size,
@@ -123,13 +100,14 @@ class MyDataModule(pl.LightningDataModule):
         test_dataset = TimeseriesDataset(self.X_test,
                                          self.y_test,
                                          seq_len=self.seq_len)
-        self.testsize = len(test_dataset)
         test_loader = DataLoader(test_dataset,
                                  batch_size=self.batch_size,
                                  shuffle=False,
                                  num_workers=self.num_workers)
 
         return test_loader
+
+
 class LSTMRegressor(pl.LightningModule):
     '''
     Standard PyTorch Lightning module:
@@ -156,21 +134,24 @@ class LSTMRegressor(pl.LightningModule):
         self.learning_rate = learning_rate
 
         self.lstm = nn.GRU(input_size=n_features,
-                            hidden_size=hidden_size,
-                            num_layers=num_layers,
-                            dropout=dropout,
-                            batch_first=True)
+                           hidden_size=hidden_size,
+                           num_layers=num_layers,
+                           dropout=dropout,
+                           batch_first=True)
         self.hidden = nn.Linear(hidden_size, hidden_size)
-        self.hidden2 = nn.Linear(hidden_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, 1)
+        self.hidden2 = nn.Linear(hidden_size, 15)
+        self.linear = nn.Linear(15, 1)
+
+
 
     def forward(self, x):
-        # lstm_out = (batch_size, seq_len, hidden_size)
+        return self.linear(self.forward_no_lastlayer(x))#[:, -1])
+
+    def forward_no_lastlayer(self,x ):
         lstm_out, _ = self.lstm(x)
-        nn_out = self.hidden(lstm_out)
+        nn_out = self.hidden(lstm_out[:,-1])
         nn_out = self.hidden2(nn_out)
-        y_pred = self.linear(nn_out[:, -1])
-        return y_pred
+        return nn_out
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
@@ -195,4 +176,3 @@ class LSTMRegressor(pl.LightningModule):
         loss = self.criterion(y_hat, y)
         self.log('test_loss', loss)
         return loss
-
